a72c8ec98e8b139661e1eded967139252576b5c3
ComedicChimera <forlornsisu@gmail.com>
2017-12-05 09:36:11 -0600

    added value cast and semantic match function

--- src/semantic_analyzer.py
+++ src/semantic_analyzer.py
@@ -1,11 +1,27 @@
+from src.ASTtools import ASTNode
+
 class SemanticAnalyzer:
     # global symbol table for the compiler
     symbol_table = {}
 
     def __init__(self):
         self.scope = 0
+        self.semantics = []
+
+    def semantic_assert(self):
+        pass
+
+    def match(self, x):
+        if isinstance(x, ASTNode):
+            if x.name == "block":
+                self.scope += 1
+                self.evaluate(x)
+                self.scope -= 1
+        return x
 
     def evaluate(self, ast):
+        for item in ast.content:
+            self.match(item)
         return ast
 
 
