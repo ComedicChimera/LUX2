4e9d8e0a9533dec91b33a0c321c231f8ca2bfcd4
ComedicChimera <forlornsisu@gmail.com>
2017-12-05 10:49:58 -0600

    added sem analyzer code

--- src/semantic_analyzer.py
+++ src/semantic_analyzer.py
@@ -1,15 +1,25 @@
 from src.ASTtools import ASTNode
 
+
+class Variable:
+    def __init__(self, name, properties):
+        self.name = name
+        self.properties = properties
+
+
 class SemanticAnalyzer:
     # global symbol table for the compiler
     symbol_table = {}
 
     def __init__(self):
         self.scope = 0
-        self.semantics = []
+        self.semantics = ["variable_decl_stmt", "assignment", "atom"]
 
-    def semantic_assert(self):
-        pass
+    def semantic_assert(self, ast, context):
+        if context == "variable_decl_stmt":
+            print(ast.content)
+            name = ast.content[1]
+            print(name)
 
     def match(self, x):
         if isinstance(x, ASTNode):
@@ -17,6 +27,8 @@ class SemanticAnalyzer:
                 self.scope += 1
                 self.evaluate(x)
                 self.scope -= 1
+            elif x.name in self.semantics:
+                self.semantic_assert(x, self.semantics[x.name])
         return x
 
     def evaluate(self, ast):
