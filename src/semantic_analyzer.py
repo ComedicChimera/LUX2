70a5681705523e2c780ad14838e2b4b836140a4d
ComedicChimera <forlornsisu@gmail.com>
2017-12-12 08:47:13 -0600

    added new classes and types

--- src/semantic_analyzer.py
+++ src/semantic_analyzer.py
@@ -1,16 +1,45 @@
 from src.ASTtools import Token, ASTNode, AST
+from enum import Enum
 
 symbol_table = {}
 
 
-class SemanticCheck:
-    def __init__(self):
-        pass
+class DataType(Enum):
+    INT = 0
+    STRING = 1
+    CHAR = 2
+    FLOAT = 3
+    BOOL = 4
 
-    @staticmethod
-    def declare(stmt):
-        for item in stmt.content:
-            print(item.__dict__)
+
+class Variable:
+    def __init__(self):
+        self.name = ""
+        self.is_const = False
+        self.data_type = DataType()
+        self.group = []
+
+
+class SemanticCheck:
+    def get_var_name(self, var):
+        return ["", []]
+
+    def get_extension_type(self, ext):
+        return ""
+
+    def declare(self, stmt):
+        var = Variable()
+        if stmt.content[0].type == "AMP":
+            var.is_const = True
+        name = self.get_var_name(stmt.content[1])
+        var.group = name[1]
+        var.name = name[0]
+        if stmt.content[2].name == "extension":
+            var.data_type = self.get_extension_type(stmt.content[2])
+
+
+
+
 
 
 class SemanticAnalyzer:
