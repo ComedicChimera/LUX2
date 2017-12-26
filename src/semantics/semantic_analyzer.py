8e84ad1ef287b11a4f896f40ea77b6820ec38332
ComedicChimera <forlornsisu@gmail.com>
2017-12-26 00:23:32 -0600

    added some semantic code

--- src/semantics/semantic_analyzer.py
+++ src/semantics/semantic_analyzer.py
@@ -1,5 +1,6 @@
 import src.semantics.variables as variables
 from src.parser.ASTtools import ASTNode
+from src.errormodule import throw
 
 symbol_table = []
 
@@ -21,24 +22,53 @@ declarations = {
 
 
 def new_scope():
-    pass
+    global scope
+    current_level = symbol_table
+    for level in range(scope):
+        current_level = current_level[-1]
+    current_level.append([])
+    scope += 1
 
 
 def add_to_symbol_table(var):
-    pass
+    current_level = symbol_table
+    for level in range(scope):
+        current_level = current_level[-1]
+    current_level.append(var)
 
 
 def construct_symbol_table(ast):
+    global scope
     for item in ast.content:
         if isinstance(item, ASTNode):
+            prev_scope = scope
             if item.name == "block":
                 new_scope()
             construct_symbol_table(item)
+            scope = prev_scope
             if item.name in declarations:
                 print(declarations[item.name](item).__dict__)
                 add_to_symbol_table(declarations[item.name](item))
 
 
+def check_context(ast, loop, func, local_scope):
+    for item in ast.content:
+        if isinstance(item, ASTNode):
+            if item.name in ["for_block", "do_block"]:
+                check_context(item, True, False, local_scope)
+            elif item.name == "func_block":
+                pass
+            elif item.name == "return_stmt" and not func:
+                throw("semantic_error", "Unable to return from region", item)
+            elif item.name in ["break_stmt", "continue_stmt"] and not loop:
+                throw("semantic_error", "Invalid loop jump", item)
+            elif item.name == "block":
+                check_context(item, loop, func, local_scope + 1)
+            else:
+                check_context(item, False, False, local_scope)
+
+
 # the main semantic checker function
 def check(ast):
     construct_symbol_table(ast)
+    check_context(ast, False, False)
