4edd3cb226a4d500240aa40f69bfe4e8efe7df5b
ComedicChimera <forlornsisu@gmail.com>
2018-04-12 11:36:59 -0500

    added dereference op untested

--- syc/icg/generators/expr.py
+++ syc/icg/generators/expr.py
@@ -1,6 +1,7 @@
 from syc.parser.ASTtools import ASTNode
 from syc.icg.action_tree import ActionNode
 import errormodule
+from util import unparse
 
 
 def generate_expr(expr):
@@ -20,7 +21,16 @@ def generate_unary_atom(u_atom):
     if len(u_atom.content) > 1:
         prefix = u_atom.content[0].content[0]
         if isinstance(prefix, ASTNode):
-            pass
+            do = len(unparse(prefix))
+            if u_atom.content[-1].pointers != do:
+                errormodule.throw('semantic_error', 'Dereferencing of non-pointer', u_atom)
+            else:
+                tree = None
+                atom = generate_atom(u_atom.content[-1])
+                for _ in range(0, do + 1):
+                    atom.data_type.pointers -= 1
+                    tree = ActionNode('Dereference', atom.data_type, atom)
+                return tree
         else:
             if prefix.type == '-':
                 atom = u_atom.content[1]
@@ -32,7 +42,7 @@ def generate_unary_atom(u_atom):
                         return ActionNode('ChangeSine', atom.data_type, atom)
                 else:
                     # throw error
-                    errormodule.throw('semantic_error', 'Unable to change sine on non-numeric type.', atom)
+                    errormodule.throw('semantic_error', 'Unable to change sine on non-numeric type.', u_atom)
     else:
         return generate_atom(u_atom.content[0])
 
